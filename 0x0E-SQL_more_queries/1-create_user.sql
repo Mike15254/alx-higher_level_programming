f918559073daf235fa680545f88b738d87e0841f
Mike15254 <mike8max10@gmail.com>
2022-08-10 22:08:25 +0300

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
--- a script that creates the database hbnt_0c_0 in MySQL sever
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
