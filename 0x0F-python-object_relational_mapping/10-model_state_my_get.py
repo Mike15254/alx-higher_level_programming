b56e3af7f2ceaee30c46e33297dd47c537366eeb
Mike15254 <mike8max10@gmail.com>
2022-09-11 20:59:11 +0300

    object-relation mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa
+Prints the State object with the name passed as argument from the db
 """
-from model_state import State, Base
-from sys import argv
+import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db = argv[3]
-    state_name = argv[4]
-
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(username, password, db),
-        pool_pre_ping=True,
-    )
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(name=state_name).first()
-    if state:
-        print("{:d}".format(state.id))
-    else:
-        print("Not found")
-    session.close()
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
